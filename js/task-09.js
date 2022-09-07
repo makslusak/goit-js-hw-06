2938d08e411b4aed343e18aec93db6b1aa479e95
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-07 19:03:32 +0300

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const buttonSwitcherRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const textRef = document.querySelector('.color');
+
+buttonSwitcherRef.addEventListener('click', colorRandomizer);
+
+function colorRandomizer(event) {
+    const randomColor = getRandomHexColor();
+    bodyRef.style.backgroundColor = randomColor;
+    textRef.textContent = randomColor;
 }
