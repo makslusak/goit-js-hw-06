b91d514ccd64b1b21171fb3087ce3eb276070c91
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-07 17:23:43 +0300

    8 task (2 version)

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,40 @@
+// const formEl = document.querySelector('.login-form');
+
+// formEl.addEventListener('submit', formValidation);
+
+// function formValidation(event) {
+//     event.preventDefault();
+//     const { email, password } = event.currentTarget.elements;
+//     const validEmail = email.value.trim();
+//     const validPassword = password.value.trim();
+
+//     if (!validEmail || !validPassword) {
+//         alert('Всі поля мають бути заповнені!');
+//     }
+//     const userProfile = {
+//         email: validEmail,
+//         password: validPassword,
+//     };
+//     console.dir(userProfile);
+//     formEl.reset();
+// }
+
 const formEl = document.querySelector('.login-form');
 
 formEl.addEventListener('submit', formValidation);
 
 function formValidation(event) {
     event.preventDefault();
-    const { email, password } = event.currentTarget.elements;
-    const validEmail = email.value.trim();
-    const validPassword = password.value.trim();
+    const formData = new FormData(event.target);
+    const userProfile = {};
+    formData.forEach((value, key) => {
+        const validValue = value.trim();
+        if (!validValue) {
+            alert('Всі поля мають бути заповнені!');
+        }
+        userProfile[key] = validValue;
+    });
 
-    if (!validEmail || !validPassword) {
-        alert('Всі поля мають бути заповнені!');
-    }
-    const userProfile = {
-        email: validEmail,
-        password: validPassword,
-    };
     console.dir(userProfile);
     formEl.reset();
 }
