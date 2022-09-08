13ed57079b1450aff06591b68bb78254092aba51
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-08 11:03:23 +0300

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,4 @@
+// 9 task
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX.Використовуй готову функцію getRandomHexColor для отримання кольору.
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputRef = document.querySelector('input');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const boxContainerRef = document.querySelector('#boxes');
+
+inputRef.addEventListener('input', getValue);
+createBtnRef.addEventListener('click', createBoxes);
+destroyBtnRef.addEventListener('click', destroyBoxes);
+let value = 0;
+function getValue(event) {
+    value = event.currentTarget.value;
+}
+function createBoxes(amount) {
+    const arr = [];
+}
+
+function destroyBoxes(event) {}
