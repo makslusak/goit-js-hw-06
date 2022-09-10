47c16cea13e8f06dcc78fecb2a96c657a0428b55
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-10 13:48:33 +0300

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,28 +1,53 @@
-// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
-// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
-
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX.Використовуй готову функцію getRandomHexColor для отримання кольору.
-// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
-
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
-const inputRef = document.querySelector('input');
-const createBtnRef = document.querySelector('[data-create]');
-const destroyBtnRef = document.querySelector('[data-destroy]');
-const boxContainerRef = document.querySelector('#boxes');
 
-inputRef.addEventListener('input', getValue);
-createBtnRef.addEventListener('click', createBoxes);
-destroyBtnRef.addEventListener('click', destroyBoxes);
-let value = 0;
-function getValue(event) {
-    value = event.currentTarget.value;
+const refs = {
+    input: document.querySelector("[type='number']"),
+    btnCreate: document.querySelector('[data-create]'),
+    btnDestroy: document.querySelector('[data-destroy]'),
+    div: document.querySelector('#controls'),
+    divBoxes: document.querySelector('#boxes'),
+};
+
+refs.btnCreate.addEventListener('click', onBtnCreateClick);
+
+function onBtnCreateClick() {
+    const countInput = refs.input.value;
+    createBoxes(countInput);
 }
+
 function createBoxes(amount) {
-    const arr = [];
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div');
+        box.style.width = size + 'px';
+        box.style.height = size + 'px';
+        box.style.backgroundColor = getRandomHexColor();
+        refs.divBoxes.append(box);
+        size += 10;
+    }
+    refs.input.value = '';
 }
 
-function destroyBoxes(event) {}
+refs.btnDestroy.addEventListener('click', onBtnDestroyClick);
+
+function onBtnDestroyClick() {
+    const countInput = refs.input.value;
+
+    if (!countInput || countInput < 0) {
+        refs.divBoxes.innerHTML = '';
+        return;
+    }
+
+    const quantity = refs.divBoxes.childElementCount;
+
+    for (let i = 0; i < countInput; i++) {
+        if (i > quantity - 1) {
+            return;
+        }
+        refs.divBoxes.lastElementChild.remove();
+    }
+}
